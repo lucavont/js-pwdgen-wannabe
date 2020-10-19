a2122efb0b0facc9d3a8fc43dd69eb837b2faf8a
Luca Cavretti <cavretiluca@live.it>
2020-10-19 17:51:42 +0200

    Finished exercise

---
+++ script.js
@@ -0,0 +1,7 @@
+var nome = prompt("Come ti chiami?");
+var cognome = prompt("Qual'è il tuo cognome?");
+var colore = prompt("Qual'è il tuo colore preferito?");
+
+// var output = nome + cognome + colore + "19";
+
+document.getElementById("output").innerHTML = nome + cognome + colore + "19";
